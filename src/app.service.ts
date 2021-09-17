24bd2bc56e587676e1880d1ffa5f1a3131c804ef
kynail <kylian.gence@epitech.eu>
2021-09-17 17:54:30 +0400

    probleme

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Logger } from '@nestjs/common';
+import {
+  SubscribeMessage,
+  WebSocketGateway,
+  OnGatewayInit,
+  WebSocketServer,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+} from '@nestjs/websockets';
+import { Socket, Server } from 'socket.io';
 
-@Injectable()
-export default class AppService {
-  getHello(): string {
-    return 'Hello World!';
+@WebSocketGateway(80, { namespace: 'events' })
+export default class AppGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  @WebSocketServer() server: Server;
+
+  private logger: Logger = new Logger('AppGateway');
+
+  @SubscribeMessage('msgToServer')
+  handleMessage(client: Socket, payload: string): void {
+    this.server.emit('msgToClient', payload);
+  }
+
+  afterInit(server: Server) {
+    this.logger.log('Init');
+  }
+
+  handleDisconnect(client: Socket) {
+    this.logger.log(`Client disconnected: ${client.id}`);
+  }
+
+  handleConnection(client: Socket, ...args: any[]) {
+    this.logger.log(`Client connected: ${client.id}`);
   }
 }
